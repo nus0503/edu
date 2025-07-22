a2f19c86dc500c44eb807394632a214fd2532847
KJS <nus0503@naver.com>
2025-07-22 17:52:52 +0900

    이미지 maxAge설정

--- src/main/java/com/company/edu/config/WebConfig.java
+++ src/main/java/com/company/edu/config/WebConfig.java
@@ -1,15 +1,22 @@
 package com.company.edu.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.Duration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        CacheControl cacheControl = CacheControl.maxAge(Duration.ofMinutes(1)).cachePublic();
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:C:/Users/nus05/uploads/");
+                .addResourceLocations("file:C:/Users/nus05/uploads/")
+                .setCacheControl(cacheControl);
     }
 }
