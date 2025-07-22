bdec01f3c97722a6c2792744af101478e7941152
KJS <nus0503@naver.com>
2025-07-23 02:41:00 +0900

    이미지 경로 동적 주입

--- src/main/java/com/company/edu/config/WebConfig.java
+++ src/main/java/com/company/edu/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.company.edu.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -10,13 +11,15 @@ import java.time.Duration;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         CacheControl cacheControl = CacheControl.maxAge(Duration.ofMinutes(1)).cachePublic();
 
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:C:/Users/nus05/uploads/")
+                .addResourceLocations("file:" + uploadDir)
                 .setCacheControl(cacheControl);
     }
 }
