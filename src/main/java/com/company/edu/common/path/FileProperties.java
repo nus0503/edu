e83913944f7b765cbfe030be50d67d113b5424c8
KJS <nus0503@naver.com>
2025-07-24 17:48:27 +0900

    value동적 주입

--- src/main/java/com/company/edu/common/path/FileProperties.java
+++ src/main/java/com/company/edu/common/path/FileProperties.java
@@ -1,6 +1,7 @@
 package com.company.edu.common.path;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Component;
 @Data
 public class FileProperties {
 
-    private String uploadDir = "C:/Users/nus05/uploads/";
+    @Value( "${file.upload-dir}")
+    private String uploadDir;
+
     private String baseUrl = "http://localhost:8080";
 }
